d220bc7aa44ef2300520a743ba9a678b16d4e6e0
RhymerRambler <ashutoshpal2006@gmail.com>
2021-03-21 23:29:06 +0530

    adding a lambda capture example

---
+++ lambda_with_capture/test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <functional>
+
+class PrintLater
+{
+  std::vector<std::function<void()>> m_printers;
+  public:
+    void log(int x) {
+      // capturing x by reference will not work because 'x' is on stack and
+      // would be long gone when lambda would actually run
+      // auto printer = [&x]() { std::cout << x << std::endl; };
+      
+      // hence, caputuring x by value
+      auto printer = [x]() { std::cout << x << std::endl; };
+      m_printers.push_back(printer);
+    }
+
+    void now() {
+      for(auto& f : m_printers) {
+        f();
+      }
+    }
+};
+
+
+int main()
+{
+  PrintLater pl;
+  pl.log(1);
+  pl.log(2);
+  pl.log(3);
+  pl.log(4);
+  std::cout << "Done logging" << std::endl;
+  pl.now();
+}
