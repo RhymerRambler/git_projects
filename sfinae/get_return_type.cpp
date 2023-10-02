a967a42ab163e469a01e75cbc3d415f979af6bf6
RhymerRambler <ashutoshpal2006@gmail.com>
2023-10-02 20:16:53 +0530

    example to extract return type of a function

---
+++ sfinae/get_return_type.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class A {
+  public:
+  std::string getStr() {
+    return "hello";
+  }
+
+  int getInt() {
+    return 361;
+  }
+
+  private:
+  std::string mStr;
+};
+
+int main()
+{
+  // declval creates a pseudo object at compile time, which can be used to get
+  // to the return type of the function
+  decltype(declval<A>().getInt()) w = 1;
+  decltype(declval<A>().getStr()) x = "uuu";
+  std::cout  << x << w << y << std::endl;
+  return 0;
+}
